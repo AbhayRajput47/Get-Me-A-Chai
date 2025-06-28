2d7d683b56f3c631f693d1844e1bd3e0342d9138
AbhayRajput47 <ar2601926@gmail.com>
2025-06-28 11:41:29 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -6,9 +6,10 @@ const connectDb = async () => {
     if (mongoose.connection.readyState >= 1) {
       return;
     }
-    const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
-      useNewUrlParser: true,
-    });
+    // const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
+    //   useNewUrlParser: true,
+    // });
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/chai`);
     console.log(`MongoDB Connected: {conn.connection.host}`);
     return conn;
   } catch (error) {
